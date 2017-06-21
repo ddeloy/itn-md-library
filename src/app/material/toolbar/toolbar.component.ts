cc4eac267e0c977bca3c03e1aa63a967e9bd4cd2
Raman Goel <ramang@dewsolutions.in>
2017-06-21 18:14:57 +0530

    adding submenu using md-menu structure

--- src/app/material/toolbar/toolbar.component.ts
+++ src/app/material/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
   private showMenu: boolean = false;
+
+  items = [{
+    text: 'Refresh'
+  }, {
+    text: 'Settings'
+  }, {
+    text: 'Help',
+    disabled: true
+  }, {
+    text: 'Sign Out'
+  }];
 
   onMenuClick() {
     this.showMenu = !this.showMenu;
