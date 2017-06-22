37ddc8ed6fca95b8fe37f72b1ac33434e81e99f2
Raman Goel <ramang@dewsolutions.in>
2017-06-22 18:04:47 +0530

    Submenu and category wise menu item styling

--- src/app/material/toolbar/toolbar.component.ts
+++ src/app/material/toolbar/toolbar.component.ts
@@ -7,17 +7,73 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
   showMenu = false;
+  open1 = false;
+  open2 = false;
+  open3 = false;
 
-  items = [{
-    text: 'Refresh'
+  open11 = false;
+  open21 = false;
+  open31 = false;
+
+  viewItems = [{
+    text: 'Category 1',
+    items: [{
+      text: 'Item 1'
+    }, {
+      text: 'Item 2'
+    }, {
+      text: 'Item 3'
+    }]
   }, {
-    text: 'Settings'
-  }, {
-    text: 'Help',
-    disabled: true
-  }, {
-    text: 'Sign Out'
+    text: 'Category 2',
+    items: [{
+      text: 'Item 1'
+    }, {
+      text: 'Item 2'
+    }, {
+      text: 'Item 3'
+    }]
   }];
+
+  editItems = [{
+    text: 'Category 1',
+    items: [{
+      text: 'Item 1'
+    }, {
+      text: 'Item 2'
+    }, {
+      text: 'Item 3'
+    }]
+  }, {
+    text: 'Category 2',
+    items: [{
+      text: 'Item 1'
+    }, {
+      text: 'Item 2'
+    }, {
+      text: 'Item 3'
+    }]
+  }, {
+    text: 'Category 3',
+    items: [{
+      text: 'Item 1'
+    }, {
+      text: 'Item 2'
+    }, {
+      text: 'Item 3'
+    }]
+  }];
+
+  // items = [{
+  //   text: 'Refresh'
+  // }, {
+  //   text: 'Settings'
+  // }, {
+  //   text: 'Help',
+  //   disabled: true
+  // }, {
+  //   text: 'Sign Out'
+  // }];
 
   onMenuClick() {
     this.showMenu = !this.showMenu;
