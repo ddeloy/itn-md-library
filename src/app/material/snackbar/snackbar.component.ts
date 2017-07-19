42a4b7ff194aca9c7437bd049baff9ac20cdb6fb
Raman Goel <ramang@dewsolutions.in>
2017-07-19 21:06:31 +0530

    styles for snackbars

--- src/app/material/snackbar/snackbar.component.ts
+++ src/app/material/snackbar/snackbar.component.ts
@@ -10,11 +10,15 @@ export class SnackbarComponent {
   message = 'Snack Bar opened.';
   actionButtonLabel = 'Retry';
   action = false;
-  setAutoHide = true;
-  autoHide = 10000;
+  setAutoHide = false;
+  autoHide = 100000;
   addExtraClass = false;
+  snackbarRef: any;
+  
 
-  constructor(public snackBar: MdSnackBar) {}
+  constructor(
+    public snackBar: MdSnackBar
+  ) {}
 
   open() {
     const config = new MdSnackBarConfig();
@@ -22,4 +26,27 @@ export class SnackbarComponent {
     config.extraClasses = this.addExtraClass ? ['party'] : null;
     this.snackBar.open(this.message, this.action && this.actionButtonLabel, config);
   }
+
+  openSnackBar(
+      message: string,
+      action: string,
+      duration: number = 2000,
+      classes: string[] = ['success', 'system']
+    ){
+      this.snackbarRef = this.snackBar.open(message, action, {
+                      duration: duration,
+                      extraClasses: ['snackbar'].concat(classes),
+                    });
+                    (<any>window).sbref = this.snackbarRef;
+      if(this.snackbarRef.containerInstance.snackBarConfig.extraClasses.includes('feedback')) {
+        let elem = document.getElementsByTagName('snack-bar-container')[0];
+        let sbElem = elem.getElementsByTagName('simple-snack-bar')[0];
+        let spanNode = document.createElement("SPAN");
+        spanNode.className = "close";
+        let textnode = document.createTextNode("close");
+        spanNode.appendChild(textnode);
+        sbElem.appendChild(spanNode);
+      }
+  }
 }
+
