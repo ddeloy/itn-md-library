df31da7878362d6a6d6c656672d8b3e1149a9e2c
Raman Goel <ramang@dewsolutions.in>
2017-07-26 18:30:26 +0530

    updates for pages of sidenav, and addition of callouts on sidenav page

--- src/app/material/sidenav/sidenav.component.ts
+++ src/app/material/sidenav/sidenav.component.ts
@@ -9,7 +9,9 @@ import { SidenavMenuItem } from './sidenav-menu-item';
 
 export class SidenavComponent implements OnInit {
   invert = false;
-  @Input() sideWidth = "sm"; // not being used for now - fixed with is defined in _seidenav.scss
+  @Input() sideWidth = "sm"; // not being used for now - fixed width is defined in _sidenav.scss
+  calloutType: string = "success";
+  autoClose: boolean = false;
 
   constructor() { }
 
@@ -27,5 +29,38 @@ export class SidenavComponent implements OnInit {
       new SidenavMenuItem('', 'Roles', '', '', null),
       new SidenavMenuItem('', 'Data Access Groups', '', '', null)
   ];
+
+  openCallout(el: any, type: string, message: string, action: string = "", autoClose: boolean = false, duration: number = 0) {
+    if(!el.opened) {
+      // do the following only if callout in bot already opened
+      el._elementRef.nativeElement.classList.add(type);
+      el._elementRef.nativeElement.innerHTML = message;
+      if(action != "") {
+        let btnNode = document.createElement("button");
+        btnNode.className = "btn-callout-action";
+        btnNode.onclick = function() {
+          // TODO: define function
+        }
+        let textnode = document.createTextNode(action);
+        btnNode.appendChild(textnode);
+        el._elementRef.nativeElement.appendChild(btnNode);
+      }
+    }
+    el.toggle();
+    //required only if auto-close needed
+    if(autoClose && el.opened) {
+      window.setTimeout(function(){el.close()}, duration);
+    }
+  }
+
+  onCalloutClose(el: any) {
+    let clList = el._elementRef.nativeElement.classList;
+    clList.remove('success');
+    clList.remove('error');
+    clList.remove('warning');
+    clList.remove('info');
+    el._elementRef.nativeElement.classList = clList;
+    el._elementRef.nativeElement.innerHTML = "";
+  }
 
 }
